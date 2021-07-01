957d4a1ed2d8cf0eef2c61e8285a952aaa6ffd4b
Dannyyash <86794735+Dannyyash@users.noreply.github.com>
2021-07-01 11:26:28 -0500

    Add files via upload

---
+++ economics/economics.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-economics',
+  templateUrl: './economics.page.html',
+  styleUrls: ['./economics.page.scss'],
+})
+export class EconomicsPage implements OnInit {
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
